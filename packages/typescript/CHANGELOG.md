f86079b0a61b3b99a3f451f9b1236db80ef68239
XiongAmao <onycat@live.cn>
2022-08-06 17:09:33 +0800

    build(typescript): release v0.2.0

--- packages/typescript/CHANGELOG.md
+++ packages/typescript/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @xiongamao/eslint-config-ts
 
+## 0.2.0
+
+### Minor Changes
+
+- improve default config
+
 ## 0.1.0
 
 ### Minor Changes
--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -3,6 +3,8 @@ module.exports = {
     '@xiongamao/eslint-config-basic',
     'plugin:@typescript-eslint/recommended'
   ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   rules: {
     'lines-between-class-members': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off'
